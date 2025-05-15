c1baa143e8b08805585596b3ad37ae68e9a476e5
dashkere <sabarkina@gmail.com>
2025-05-15 13:13:02 +0300

    Пробные логи

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -17,7 +17,7 @@ import poster2 from '../assets/poster2.jpg';
 import poster3 from '../assets/poster3.jpg';
 import poster4 from '../assets/poster4.jpg';
 import poster5 from '../assets/poster5.jpg';
-
+import { logEvent } from '../logger';
 const posters = [poster1, poster2, poster3, poster4, poster5]; 
 
 const genres = [
